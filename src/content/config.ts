60a8b1419ba25541dcf0c8749cc120667138d24d
Georgios Piskas <gpiskas@gmail.com>
2024-02-03 22:33:54 +0100

    project image

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,6 +34,10 @@ const projects = defineCollection({
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
       tags: z.array(z.string()).default(["project"]),
+      projectImage: image()
+        .refine(img => img.width == img.height && img.width >= 200, {
+          message: "Project image must be square and at least 200 pixels!",
+        }),
       ogImage: image()
         .refine(img => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
--- src/content/projects/streamdog.md
+++ src/content/projects/streamdog.md
@@ -1,6 +1,7 @@
 ---
 title: Streamdog
 description: Streamdog...
+projectImage: ../../assets/images/projects/streamdog.png
 author: Georgios Piskas
 pubDatetime: 2024-01-29T22:49:39
 slug: streamdog
