4359b84ea5a19ca9e15b1d5959d0d6265c6be0f2
Georgios Piskas <gpiskas@gmail.com>
2024-02-15 19:25:19 +0100

    jampack config

--- jampack.config.mjs
+++ jampack.config.mjs
@@ -1,25 +1,28 @@
 export default {
+  image: {
+    compress: false,
     jpeg: {
       options: {
-        quality: 10,
+        quality: 95,
         mozjpeg: true,
       },
     },
     png: {
       options: {
-        compressionLevel: 9,
+        compressionLevel: 2,
       },
     },
     webp: {
       options_lossless: {
         effort: 6,
-        quality: 10,
+        quality: 95,
         mode: 'lossless',
       },
       options_lossly: {
         effort: 6,
-        quality: 10,
+        quality: 95,
         mode: 'lossly',
       },
     },
-  };
+  }
+};
